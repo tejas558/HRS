9f9bf1dceab37c9b22cf85aa93a38164cf204efa
tejas <tejas.professional.558@gmail.com>
2023-12-27 11:28:23 +0900

    updated staff page

---
+++ src/app/staff/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+      <div className="">
+        <div className="center">
+            Welcome Staff!
+        </div>
+        <div className="center">
+            Click here to Check In a customer.
+        </div>
+        <div className="center">
+            Click here to Check out a customer.
+        </div>
+      </div>
+    )
+  }
+  
